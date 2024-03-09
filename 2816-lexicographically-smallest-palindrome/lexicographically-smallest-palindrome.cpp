2757e7d5f1de7ebd040dc391e607dfb2b25841ca
Dharsan-PG <104558742+Dharsan-PG@users.noreply.github.com>
2024-03-09 16:39:23 +0530

    Time: 28 ms (87.92%) | Memory: 18.4 MB (63.69%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j]) {
+                if (int(s[i]) > int(s[j]))
+                    s[i] = s[j];
+                else
+                    s[j] = s[i];
+            }
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
